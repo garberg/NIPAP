63b59127530778abb6009d38b9a96c76dcc5111a
Lukas Garberg <lukas@spritelink.net>
2014-02-06 17:34:53 +0100

    Introduced AngularJS
    The /vrf/list page is now rendered with AngularJS. The drop-down menu
    does not work yet though :)

---
+++ nipap-www/nipapwww/public/controllers.js
@@ -0,0 +1,39 @@
+var nipapApp = angular.module('nipapApp', []);
+
+nipapApp.controller('VRFListController', function ($scope, $http) {
+
+    $http.get('/xhr/list_vrf').success(function (data) {
+        if (data.hasOwnProperty('error')) {
+            // display some error message
+        } else {
+            $scope.vrfs = data;
+        }
+    });
+
+    $scope.showVrfMenu = function (vrf_id, vrf_rt) {
+        var menu = getPopupMenu($(this), 'VRF', vrf_id);
+
+        menu.append('<a href="/vrf/edit/' + vrf_id + '">Edit</a>');
+        menu.append('<a id="vrf_remove' + vrf_id + '" href="/vrf/remove/' + vrf_id + '">Remove</a>');
+
+        $('#vrf_remove' + vrf_id).click(function(e) {
+            e.preventDefault();
+            var dialog = showDialogYesNo('Really remove VRF?', 'Are you sure you want to remove the VRF "' + vrf_rt + '"?',
+            function () {
+                var data = {
+                    'id': vrf_id
+                };
+                $.getJSON('/xhr/remove_vrf', data, vrfRemoved);
+
+                hidePopupMenu();
+                dialog.dialog('close');
+
+            });
+        });
+
+        menu.slideDown('fast');
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+});
