64463bdc33a7693d606af348aba1ba557fe3e03b
Lukas Garberg <lukas@spritelink.net>
2014-02-07 04:53:10 +0100

    Added comments to controllers.js

--- nipap-www/nipapwww/public/controllers.js
+++ nipap-www/nipapwww/public/controllers.js
@@ -1,7 +1,18 @@
+/*
+ * Controllers for the NIPAP AngularJS app
+ */
+
+/*
+ * Define nipapApp angular application
+ */
 var nipapApp = angular.module('nipapApp', []);
 
+/*
+ * VRFListController - used to list VRFs on the /vrf/list-page
+ */
 nipapApp.controller('VRFListController', function ($scope, $http) {
 
+    // Fetch VRFs from backend
     $http.get('/xhr/list_vrf').success(function (data) {
         if (data.hasOwnProperty('error')) {
             // display some error message
