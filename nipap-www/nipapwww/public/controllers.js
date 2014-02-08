dd101c9d7996767eb1b18d39dd769796cfa110a4
Lukas Garberg <lukas@spritelink.net>
2014-02-08 05:24:26 +0100

    VRF popup menu now rendered with Angular
    The VRF popup menu is now rendered with the angular-ui component
    'dropdown'. There is still some work to do before it's finished, such as
    adding the confirmation dialog and adressing JavaScript errors...
    
    Changed to a non-minified angular.js during development.

--- nipap-www/nipapwww/public/controllers.js
+++ nipap-www/nipapwww/public/controllers.js
@@ -5,7 +5,7 @@
 /*
  * Define nipapApp angular application
  */
-var nipapApp = angular.module('nipapApp', []);
+var nipapApp = angular.module('nipapApp', [ 'ui.bootstrap.dropdown' ]);
 
 /*
  * VRFListController - used to list VRFs on the /vrf/list-page
@@ -20,31 +20,5 @@ nipapApp.controller('VRFListController', function ($scope, $http) {
             $scope.vrfs = data;
         }
     });
-
-    $scope.showVrfMenu = function (vrf_id, vrf_rt) {
-        var menu = getPopupMenu($(this), 'VRF', vrf_id);
-
-        menu.append('<a href="/vrf/edit/' + vrf_id + '">Edit</a>');
-        menu.append('<a id="vrf_remove' + vrf_id + '" href="/vrf/remove/' + vrf_id + '">Remove</a>');
-
-        $('#vrf_remove' + vrf_id).click(function(e) {
-            e.preventDefault();
-            var dialog = showDialogYesNo('Really remove VRF?', 'Are you sure you want to remove the VRF "' + vrf_rt + '"?',
-            function () {
-                var data = {
-                    'id': vrf_id
-                };
-                $.getJSON('/xhr/remove_vrf', data, vrfRemoved);
-
-                hidePopupMenu();
-                dialog.dialog('close');
-
-            });
-        });
-
-        menu.slideDown('fast');
-        e.preventDefault();
-        e.stopPropagation();
-    }
 
 });
